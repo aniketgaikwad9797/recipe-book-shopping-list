2ee61944da557614840e8849a879aaaa276cf83d
Aniket Gaikwad <aniketgaikwad9797@gmail.com>
2023-08-28 17:17:20 +0530

    Test recipes added in recipe list

--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
-
+  recipeList: Recipe[] = [
+    new Recipe(
+      'TestRecipe',
+      'Description about the recipe',
+      'https://www.jocooks.com/wp-content/uploads/2019/07/garlic-and-paprika-chicken-1-14-1229x1536.jpg'
+    ),
+    new Recipe(
+      'TestRecipe',
+      'Description about the recipe',
+      'https://www.jocooks.com/wp-content/uploads/2019/07/garlic-and-paprika-chicken-1-14-1229x1536.jpg'
+    ),
+  ];
 }
