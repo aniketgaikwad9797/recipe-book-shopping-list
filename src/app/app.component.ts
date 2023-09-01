af25747ddb35b090f8662460fdf57755b89c68c9
Aniket Gaikwad <aniketgaikwad9797@gmail.com>
2023-09-02 00:31:59 +0530

    default recipe boo view in app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'RecipeBookShoppingList';
-  showRecipeBook = false;
+  showRecipeBook = true;
   showShoppingList = false;
 
   onSelectDisplay(selectedElementFlags: {
