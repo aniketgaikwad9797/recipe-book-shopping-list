932e4680f45a7061a5cf0833564c1bd437ea50d9
Aniket Gaikwad <aniketgaikwad9797@gmail.com>
2023-10-02 16:02:24 +0530

    getting rid of local references

--- src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -12,14 +12,9 @@ import { ShoppingService } from 'src/app/shared/services/shopping-list.service';
   styleUrls: ['./shopping-list-edit.component.css'],
 })
 export class ShoppingListEditComponent {
-  @ViewChild('ingredientName') ingName: ElementRef;
-  @ViewChild('ingredientAmount') ingAmount: ElementRef;
-
   constructor(private shoppingService: ShoppingService) {}
 
   onAddIngredient() {
-    const ingredientName = this.ingName.nativeElement.value;
-    const ingredientAmount = this.ingAmount.nativeElement.value;
     const newIngredient = new Ingredient(ingredientName, ingredientAmount);
     this.shoppingService.addShoppingIngredient(newIngredient);
     //this.ingredientAdded.emit(newIngredient);
