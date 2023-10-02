0cfd18a731863c6364f4a7f42104626df8df009e
Aniket Gaikwad <aniketgaikwad9797@gmail.com>
2023-10-02 16:09:34 +0530

    adding the sopping list form

--- src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,8 +1,5 @@
-import {
-  Component,
-  ElementRef,
-  ViewChild,
-} from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingService } from 'src/app/shared/services/shopping-list.service';
 
@@ -14,8 +11,10 @@ import { ShoppingService } from 'src/app/shared/services/shopping-list.service';
 export class ShoppingListEditComponent {
   constructor(private shoppingService: ShoppingService) {}
 
-  onAddIngredient() {
-    const newIngredient = new Ingredient(ingredientName, ingredientAmount);
+  onAddIngredient(editForm: NgForm) {
+    console.log(editForm);
+    const values = editForm.value;
+    const newIngredient = new Ingredient(values.nameField, values.amountField);
     this.shoppingService.addShoppingIngredient(newIngredient);
     //this.ingredientAdded.emit(newIngredient);
   }
