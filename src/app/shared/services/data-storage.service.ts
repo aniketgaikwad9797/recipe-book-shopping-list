010429580b9e485c444bafd0602134b266e73af4
Aniket Gaikwad <aniketgaikwad9797@gmail.com>
2023-10-11 01:18:28 +0530

    Transforming response data

--- src/app/shared/services/data-storage.service.ts
+++ src/app/shared/services/data-storage.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { RecipeService } from './recipe.service';
 import { Recipe } from 'src/app/recipe-book/recipe.model';
+import { map } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
@@ -21,10 +22,20 @@ export class DataStorageService {
 
   fetchRecipes() {
     return this.http
-      .get(
+      .get<Recipe[]>(
         'https://recipe-book-shopping-lis-10178-default-rtdb.firebaseio.com/recipes.json'
       )
-      .subscribe((recipeList: Recipe[]) => {
+      .pipe(
+        map((recipes) => {
+          return recipes.map((recipes) => {
+            return {
+              ...recipes,
+              ingredients: recipes.ingredients ? recipes.ingredients : [],
+            };
+          });
+        })
+      )
+      .subscribe((recipeList) => {
         this.recipeService.updateRecipes(recipeList);
       });
   }
