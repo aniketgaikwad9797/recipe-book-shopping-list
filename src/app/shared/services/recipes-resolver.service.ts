2b6325004c2ca50a60f8e6fef51904ca6703ac71
Aniket Gaikwad <aniketgaikwad9797@gmail.com>
2023-10-11 01:49:05 +0530

    fixing recipe edit bug

--- src/app/shared/services/recipes-resolver.service.ts
+++ src/app/shared/services/recipes-resolver.service.ts
@@ -5,11 +5,17 @@ import {
   RouterStateSnapshot,
 } from '@angular/router';
 import { DataStorageService } from './data-storage.service';
+import { RecipeService } from './recipe.service';
 
 @Injectable({ providedIn: 'root' })
 export class RecipesResolverService implements Resolve<any> {
-  constructor(private dataStorageService: DataStorageService) {}
+  constructor(
+    private dataStorageService: DataStorageService,
+    private recipeService: RecipeService
+  ) {}
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    return this.dataStorageService.fetchRecipes();
+    const recipeList = this.recipeService.getRecipes();
+    if (recipeList.length === 0) return this.dataStorageService.fetchRecipes();
+    else return recipeList;
   }
 }
