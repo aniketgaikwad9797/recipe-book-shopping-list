8878709aa253f08db941f9f12965b0abfae8b38b
Aniket Gaikwad <aniketgaikwad9797@gmail.com>
2023-10-11 00:56:05 +0530

    setting empty recipe list instead of hardcoded one

--- src/app/shared/services/recipe.service.ts
+++ src/app/shared/services/recipe.service.ts
@@ -4,20 +4,21 @@ import { Subject } from 'rxjs';
 
 export class RecipeService {
   recipeListChanged = new Subject<Recipe[]>();
-  private completeRecipeList: Recipe[] = [
-    new Recipe(
-      'Big Fat Burger',
-      'Tasty Chicken filled fat Burger!',
-      'https://cdn-cmjom.nitrocdn.com/FpMsHpAgoVrRMnuAdmBhGkyiizdsWlSU/assets/images/optimized/rev-dd2c928/wp-content/uploads/2018/09/fb18_FatCheeseBeer.png.webp',
-      [new Ingredient('chicken breast', 1), new Ingredient('Bun', 2)]
-    ),
-    new Recipe(
-      'Nesquick Chocolate Wafle',
-      'Chocolate coated tasty Waffles with strawberries',
-      'https://www.nestle-cereals.com/global/sites/g/files/qirczx806/files/srh_recipes/a3858458e3a662d06925210462b6471b.jpg',
-      [new Ingredient('Egs', 6), new Ingredient('Flour', 5)]
-    ),
-  ];
+  // private completeRecipeList: Recipe[] = [
+  //   new Recipe(
+  //     'Big Fat Burger',
+  //     'Tasty Chicken filled fat Burger!',
+  //     'https://cdn-cmjom.nitrocdn.com/FpMsHpAgoVrRMnuAdmBhGkyiizdsWlSU/assets/images/optimized/rev-dd2c928/wp-content/uploads/2018/09/fb18_FatCheeseBeer.png.webp',
+  //     [new Ingredient('chicken breast', 1), new Ingredient('Bun', 2)]
+  //   ),
+  //   new Recipe(
+  //     'Nesquick Chocolate Wafle',
+  //     'Chocolate coated tasty Waffles with strawberries',
+  //     'https://www.nestle-cereals.com/global/sites/g/files/qirczx806/files/srh_recipes/a3858458e3a662d06925210462b6471b.jpg',
+  //     [new Ingredient('Egs', 6), new Ingredient('Flour', 5)]
+  //   ),
+  // ];
+  private completeRecipeList: Recipe[] = [];
 
   getRecipes() {
     //slice returns a copy of the exact array
