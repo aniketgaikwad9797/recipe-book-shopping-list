576dcb379236792a414b6147e2097ffa618295f7
e5658897 <aniket.rajeshgaikwad@fisglobal.com>
2025-02-09 00:39:23 +0530

    configuring recipe module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,13 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
-import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipe-book/recipe-list/recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
-import { RecipeBookComponent } from './recipe-book/recipe-book.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingService } from './shared/services/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
-import { RecipeStartComponent } from './recipe-book/recipe-start/recipe-start.component';
-import { EditRecipeComponent } from './recipe-book/edit-recipe/edit-recipe.component';
 import { RecipeService } from './shared/services/recipe.service';
 import { HttpClientModule} from '@angular/common/http'
-import { DropdownDirectiveRevision } from './shared/dropdown-dir-revision';
+import { RecipeModule } from './recipe-book/recipe.module';
 
 @NgModule({
   declarations: [
@@ -24,16 +18,9 @@ import { DropdownDirectiveRevision } from './shared/dropdown-dir-revision';
     HeaderComponent,
     ShoppingListComponent,
     ShoppingListEditComponent,
-    RecipeListComponent,
-    RecipeItemComponent,
-    RecipeDetailComponent,
-    RecipeBookComponent,
-    DropdownDirective,
-    RecipeStartComponent,
-    EditRecipeComponent,
-    DropdownDirectiveRevision
+    DropdownDirective
   ],
-  imports: [BrowserModule, FormsModule, HttpClientModule,  AppRoutingModule, ReactiveFormsModule],
+  imports: [BrowserModule, FormsModule, HttpClientModule,  AppRoutingModule, ReactiveFormsModule, RecipeModule],
   providers: [ShoppingService, RecipeService],
   bootstrap: [AppComponent],
 })
--- src/app/recipe-book/recipe.module.ts
+++ src/app/recipe-book/recipe.module.ts
@@ -1,6 +1,31 @@
-import { NgModule } from "@angular/core";
+import { NgModule } from '@angular/core';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { RecipeBookComponent } from './recipe-book.component';
+import { AppRoutingModule } from '../app-routing.module';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RecipeStartComponent } from './recipe-start/recipe-start.component';
+import { EditRecipeComponent } from './edit-recipe/edit-recipe.component';
+import { DropdownDirectiveRevision } from '../shared/dropdown-dir-revision';
 
 @NgModule({
-    declarations:[]
+  declarations: [
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    RecipeBookComponent,
+    RecipeStartComponent,
+    EditRecipeComponent,
+    DropdownDirectiveRevision,
+  ],
+  imports: [AppRoutingModule, CommonModule, ReactiveFormsModule],
+  exports: [
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    RecipeBookComponent,
+  ],
 })
-export class RecipeModule{}
+export class RecipeModule {}
